52f2000c0f27944312fe9c34cf1ecac638851d04
LemanRus <wlad163163@gmail.com>
2023-01-31 16:05:45 +0400

    Base app skeleton

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+import screens
+
+
+class RadioMan(MDApp):
+    def build(self):
+        Window.bind(on_keyboard=self.android_back_click)
+        Window.softinput_mode = 'below_target'
+        self.theme_cls.primary_palette = "DeepPurple"
+        kv = os.listdir("kv")
+        for kv_file in kv:
+            if kv_file != "misc.kv":
+                Builder.load_file(f"kv/{kv_file}")
+        return Builder.load_file("kv/main.kv")
+
+    def android_back_click(self, window, key, *args):
+        pass
+
+
+RadioMan().run()
---
+++ screens.py
@@ -0,0 +1,18 @@
+from kivymd.uix.screen import MDScreen
+
+
+class MarkingsScreen(MDScreen):
+    pass
+
+
+class CalculationsScreen(MDScreen):
+    pass
+
+
+class HandbookScreen(MDScreen):
+    pass
+
+
+class HelpScreen(MDScreen):
+    pass
+
