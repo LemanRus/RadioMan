cf04716d20c6a71cc67753009bc682bacc8d003a
LemanRus <wlad163163@gmail.com>
2023-02-02 17:00:53 +0400

    Design 2ns toplevel screen

--- screens.py
+++ screens.py
@@ -1,4 +1,9 @@
+from kivy.app import App
+from kivy.properties import NumericProperty
+from kivymd.uix.bottomnavigation import MDBottomNavigation
+from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.screen import MDScreen
+from kivymd.uix.scrollview import MDScrollView
 
 
 class MarkingsScreen(MDScreen):
