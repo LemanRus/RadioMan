58194966bc60aab0dd1a8a4e3e74152d1826be67
LemanRus <wlad163163@gmail.com>
2023-02-03 16:57:30 +0400

    Refactor labels to be shrinkable

--- screens.py
+++ screens.py
@@ -1,9 +1,4 @@
-from kivy.app import App
-from kivy.properties import NumericProperty
-from kivymd.uix.bottomnavigation import MDBottomNavigation
-from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.screen import MDScreen
-from kivymd.uix.scrollview import MDScrollView
 
 
 class MarkingsScreen(MDScreen):
